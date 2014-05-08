0863bfe05904bffc6329be7dbf9691af703e655b
Clayton Burlison <clburlison@gmail.com>
2014-05-08 07:39:22 -0500

    Update to AutoDMG v1.4

--- manifests/init.pp
+++ manifests/init.pp
@@ -6,6 +6,6 @@
 class autodmg {
   package { 'AutoDMG':
     provider => 'appdmg',
-    source   => 'https://github.com/MagerValp/AutoDMG/releases/download/v1.2/AutoDMG-1.2.dmg'
+    source   => 'https://github.com/MagerValp/AutoDMG/releases/download/v1.4/AutoDMG-1.4.dmg'
   }
 }
